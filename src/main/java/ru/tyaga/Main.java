5b81b4d8af6213d9c3f90fd5ebba55bdee0caf93
Tonkorg <Thomasvill16@inbox.ru>
2024-03-18 17:35:06 +0400

    parser

--- src/main/java/ru/tyaga/Main.java
+++ src/main/java/ru/tyaga/Main.java
@@ -7,35 +7,38 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-            String url = "https://www.krylatskoye.ru/content/ratings/2021/09/0928.html";
-            try {
-                // Получаем HTML-документ с помощью Jsoup
-                Document doc = Jsoup.connect(url).get();
+        String url = "https://www.krylatskoye.ru/content/ratings/2021/09/0928.html";
+        try {
+            // Получаем HTML-документ с помощью Jsoup
+            Document doc = Jsoup.connect(url).get();
 
-                // Извлекаем таблицу по ее классу
-                Elements table = doc.getElementsByClass("table_free");
+            // Извлекаем таблицу по ее классу
+            Elements table = doc.getElementsByClass("table_free");
 
-                // Проверяем, что таблица была найдена
-                if (table != null) {
-                    // Извлекаем строки таблицы
-                    Elements rows = table.select("tr");
-                    for (Element row : rows) {
-                        // Извлекаем ячейки из строки
-                        Elements cells = row.select("td");
-                        System.out.print(cells.text() + "\t");
+            // Проверяем, что таблица была найдена
+            if (table != null && !table.isEmpty()) {
+                // Извлекаем строки таблицы
+                Elements rows = table.select("tr");
+                for (Element row : rows) {
+                    // Извлекаем ячейки из строки
+                    Elements cells = row.select("td");
 
-                        System.out.println(); // Переход на новую строку после каждой строки таблицы
+                    // Проверяем, что у нас есть как минимум две ячейки (столбца) в текущей строке
+                    if (cells.size() >= 2) {
+                        // Выводим содержимое второго столбца (индекс 1)
+                        System.out.print(cells.get(1).text() + "\t");
                     }
-                } else {
-                    System.out.println("Таблица не найдена");
+
+                    System.out.println(); // Переход на новую строку после каждой строки таблицы
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+            } else {
+                System.out.println("Таблица не найдена");
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
